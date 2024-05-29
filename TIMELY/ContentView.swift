63d27f5d04b2b507c0ca022c395bed9774dc59ee
tabletopjosh <joshuaarounI@gmail.com>
2024-05-29 16:07:14 -0400

    Initial Commit

---
+++ TIMELY/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TIMELY
+//
+//  Created by Sa38 on 5/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TIMELY/TIMELYApp.swift
@@ -0,0 +1,17 @@
+//
+//  TIMELYApp.swift
+//  TIMELY
+//
+//  Created by Sa38 on 5/29/24.
+//
+
+import SwiftUI
+
+@main
+struct TIMELYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
